d55c5c39495dc9ec93e9f5e14c08c11ce6b844b5
stephanie <stephanie.fadel@hotmail.com>
2023-04-19 14:49:32 +0200

    Log in pop up

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,62 @@
-const Homepage = ({ }) => {
+import { useState } from "react";
 
-    return (
-        <nav className="navbar">
-        <ul className="nav-links">
-        </ul>
+const Homepage = ({}) => {
+  const [showLogin, setShowLogin] = useState(false); 
+  const [username, setUsername] = useState(""); 
+
+  // Funktion som öppnar popup-rutan
+  const handleLoginClick = () => {
+    setShowLogin(true);
+  };
+
+  // Funktion som stänger popup-rutan
+  const handleLoginClose = () => {
+    setShowLogin(false);
+  };
+
+  // Funktion som hanterar ändringar i användarnamnsfältet
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleNextClick = () => {
+    // Här kan du lägga till logik för att kontrollera användarnamnet och eventuellt visa nästa steg i inloggningen
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <ul className="nav-links"></ul>
         <div className="nav-buttons">
-          <button>Log in</button>
+          <button onClick={handleLoginClick}>Log in</button>
           <button>Register</button>
         </div>
       </nav>
-    )
-}
 
+      {/* Popup-rutan */}
+      {showLogin && (
+  
+          <div className="login-modal-content">
+            <span className="login-close" onClick={handleLoginClose}>
+              &times;
+            </span>
+            <h2 className="center">Log in</h2>
+            <div className="content-login">
+            <input
+            placeholder="Username"
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+            {/* Nästa-knappen */}
+            <button onClick={handleNextClick}>Nästa</button>
+          </div>
+          </div>
+      )}
+    </div>
+  );
+};
 
-export default Homepage; 
+export default Homepage;
