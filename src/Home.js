a911ab2d7ec2051332edf67d46224f352fe817cf
stephanie <stephanie.fadel@hotmail.com>
2023-04-19 13:44:00 +0200

    Added navbar

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+const Homepage = ({ }) => {
+
+    return (
+        <nav className="navbar">
+        <ul className="nav-links">
+        </ul>
+        <div className="nav-buttons">
+          <button>Log in</button>
+          <button>Register</button>
+        </div>
+      </nav>
+    )
+}
+
+
+export default Homepage; 
