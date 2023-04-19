cafaae4511f98b31a01dea6b26c0b92bafb005a2
stephanie <stephanie.fadel@hotmail.com>
2023-04-19 17:05:23 +0200

    Styling popup login and nav bar > footer

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 const Homepage = ({}) => {
-  const [showLogin, setShowLogin] = useState(false); 
-  const [username, setUsername] = useState(""); 
+  const [showLogin, setShowLogin] = useState(false);
+  const [username, setUsername] = useState("");
 
   const handleLoginClick = () => {
     setShowLogin(true);
@@ -16,41 +16,48 @@ const Homepage = ({}) => {
     setUsername(event.target.value);
   };
 
-  const handleNextClick = () => {
-  };
+  const handleNextClick = () => {};
 
   return (
-    <div>
-      <nav className="navbar">
-        <ul className="nav-links"></ul>
-        <div className="nav-buttons">
-          <button onClick={handleLoginClick}>Log in</button>
-          <button>Register</button>
-        </div>
-      </nav>
+    <div className="page-wrapper">
+      <div className="content-wrapper">
+      </div>
 
       {/* Popup-rutan */}
       {showLogin && (
-  
+        <div className="login-modal-wrapper">
           <div className="login-modal-content">
             <span className="login-close" onClick={handleLoginClose}>
               &times;
             </span>
-            <h2 className="center">Log in</h2>
+            <h2 className="center">Log in on Twitter</h2>
             <div className="content-login">
-            <input
-            placeholder="Username"
-              type="text"
-              id="username"
-              name="username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-            {/* Nästa-knappen */}
-            <button onClick={handleNextClick}>Nästa</button>
+              <input
+                placeholder="Username"
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+              {/* Nästa-knappen */}
+              <button onClick={handleNextClick}>Nästa</button>
+              <p>Har du inget konto? Registrera dig</p>
+            </div>
           </div>
-          </div>
+        </div>
       )}
+
+      {/* Footer */}
+      <footer className="footer">
+        <div className="content-wrapper">
+          <ul className="nav-links"></ul>
+          <div className="nav-buttons">
+            <button className="login-btn-footer" onClick={handleLoginClick}>Log in</button>
+            <button>Register</button>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
